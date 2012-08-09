315301b8baff554e9903c5bee7a3586232ed6924
Ryan Feng <odayfans@gmail.com>
2012-08-09 14:33:01 -0400

    Fix gensystags.sh

--- gensystags.sh
+++ gensystags.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
-TAGS_BIN="/usr/bin/ctags"
+TAGS_BIN="`which ctags`"
 SYSTAGS_OUT="$HOME/.vim/systags"
-TAGFLIST="/usr/include/*"
-RTAGFLIST="/usr/local/include/gtk-2.0/ \
-/usr/include/sys/ \
-/usr/include/bits/ \
-/usr/local/include/glib-2.0/ \
-/usr/local/include/mysql/ \
-/usr/include/net \
-/usr/include/netinet \
-/usr/local/include/cairo \
-/usr/local/include/curl"
+TAGFLIST=""
+RTAGFLIST="/usr/include/ \
+/opt/local/include"
 
-CTAGS_FLAGS="–c-kinds=+p –fields=+iaS –extra=+q –language-force=c"
+CTAGS_FLAGS='--c-kinds=+p --fields=+iaS --extra=+q --language-force=c'
 
-#[[ -x $CTAGS_BIN ]] && \
-#$CTAGS_BIN –verbose $CTAGS_FLAGS -f $SYSTAGS_OUT $TAGFLIST -R $RTAGFLIST
-ctags –c-kinds=+p –fields=+S -f $SYSTAGS_OUT $TAGFLIST -R $RTAGFLIST
+[[ -x $TAGS_BIN ]] && \
+$TAGS_BIN --verbose $CTAGS_FLAGS -f $SYSTAGS_OUT $TAGFLIST -R $RTAGFLIST
+#ctags –c-kinds=+p –fields=+S -f $SYSTAGS_OUT $TAGFLIST -R $RTAGFLIST
 ls -lh $SYSTAGS_OUT
