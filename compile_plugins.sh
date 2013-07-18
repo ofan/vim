7ec1a7d7ef23b2d0b363d3b426e4a1b7b8f0312e
Ryan Feng <odayfans@gmail.com>
2013-07-17 21:01:36 -0400

    Add YouCompleteMe and vimproc

--- compile_plugins.sh
+++ compile_plugins.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 # Here's a list of all plugins that need to compile before use.
-# command-t
+# youcompleteme
+echo Compiling YouCompleteMe...
+cd ~/.vim/bundle/YouCompleteMe
+./install.sh
+cd -
 # vimproc
-
+echo Compiling vimproc...
+cd ~/.vim/bundle/vimproc
+make -f make_mac.mak
+cd -
